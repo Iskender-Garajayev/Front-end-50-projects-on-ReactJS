0a277b48f8b1e7341511e9874c3f15875d3b7f4a
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-08-11 07:12:03 +0300

    update

--- project-1-market/src/App.js
+++ project-1-market/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header className=' text-blue-500'/>
+    <div>
+      <Header/>
       <Footer/>
     </div>
   );
--- project-1-market/src/components/header/Header.js
+++ project-1-market/src/components/header/Header.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <div className="header_logo"></div>
 
-export default Header
+      <div className="header_serch"></div>
+
+      <div className="header_nav">
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Hello Guest</span>
+          <span className="nav_itemLineTwo">Sing In</span>
+        </div>
+      </div>
+      <div className="header_nav">
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
+        </div>
+      </div>
+      <div className="header_nav">
+        <div className="nav_item">
+          <ShoppingBasketOutlinedIcon/>
+          <span className="nav_itemLineTwo">0</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
