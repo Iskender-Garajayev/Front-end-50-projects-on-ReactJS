786067a8a1e8df64e874300b8ae4adb9d0c84b34
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-08-11 17:19:06 +0300

    update

--- project-1-market/src/components/header/Header.js
+++ project-1-market/src/components/header/Header.js
@@ -1,28 +1,31 @@
 import React from "react";
-import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
+import ShoppingBasketOutlinedIcon from "@mui/icons-material/ShoppingBasketOutlined";
+import MuseumOutlinedIcon from "@mui/icons-material/MuseumOutlined";
 
 const Header = () => {
   return (
-    <div>
+    <div className=" flex justify-between">
       <div className="header_logo"></div>
-
+      <MuseumOutlinedIcon />
       <div className="header_serch"></div>
-
-      <div className="header_nav">
+      <input
+        className=" rounded-2xl border-black"
+        placeholder="Search"
+        type="text"
+      />
+      <div className="header_nav  justify-between">
         <div className="nav_item">
           <span className="nav_itemLineOne">Hello Guest</span>
           <span className="nav_itemLineTwo">Sing In</span>
         </div>
-      </div>
-      <div className="header_nav">
+
         <div className="nav_item">
           <span className="nav_itemLineOne">Your</span>
           <span className="nav_itemLineTwo">Shop</span>
         </div>
-      </div>
-      <div className="header_nav">
+
         <div className="nav_item">
-          <ShoppingBasketOutlinedIcon/>
+          <ShoppingBasketOutlinedIcon />
           <span className="nav_itemLineTwo">0</span>
         </div>
       </div>
