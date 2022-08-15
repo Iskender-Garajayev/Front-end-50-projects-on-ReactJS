09078e6b2f01c1735a3772924bca1761b7cacb89
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-08-16 01:11:00 +0300

    update

--- weather-app/src/components/search/Search.js
+++ weather-app/src/components/search/Search.js
@@ -1,27 +1,46 @@
-import React, { useState } from 'react'
-import {AsyncPaginate} from 'react-select-async-paginate'
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL } from "../../Api";
+import { geoApiOptions } from "../../Api";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
 
-  const [search, setSearch] = useState(null)
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
 
-  const handleOnChange = (searchData) =>{
+  const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
-  }
-
-
+  };
 
   return (
     <div>
-      <AsyncPaginate  
-        placeholder='Search for city'
+      <AsyncPaginate
+        placeholder="Search for city"
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
+        loadOptions={loadOptions}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
