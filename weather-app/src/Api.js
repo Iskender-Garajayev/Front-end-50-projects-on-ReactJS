e8f4c43e5b6a8010ce6886e5bc50e5595a08a904
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-08-17 01:19:26 +0300

    update

--- weather-app/src/Api.js
+++ weather-app/src/Api.js
@@ -1,4 +1,15 @@
 export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5a12947e86mshe5876e48e705d92p161626jsnbdced970dd14",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '5a12947e86mshe5876e48e705d92p161626jsnbdced970dd14',
@@ -6,5 +17,7 @@ export const geoApiOptions = {
 	}
 };
 
- export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
-
+fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
--- weather-app/src/components/search/Search.js
+++ weather-app/src/components/search/Search.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL } from "../../Api";
-import { geoApiOptions } from "../../Api";
+import { GEO_API_URL, geoApiOptions } from "../../Api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
-      .then((response) => {
-        return {
-          options: response.data.map((city) => {
-            return {
-              value: `${city.latitude} ${city.longitude}`,
-              label: `${city.name}, ${city.countryCode}`,
-            };
-          }),
-        };
-      })
+      .then((response) => console.log(response))
+      //   return {
+      //     options: response.data.map((city) => {
+      //       return {
+      //         value: `${city.latitude} ${city.longitude}`,
+      //         label: `${city.name}, ${city.countryCode}`,
+      //       };
+      //     }),
+      //   };
+      // })
       .catch((err) => console.error(err));
   };
 
