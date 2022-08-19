19dbb87badd9a8dd822dd2b9f1eb5643994d70aa
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-08-19 08:22:27 +0300

    create movie app home component

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,9 +1,22 @@
+import React, {useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header/>
+        <Routes>
+          <Route index element={<Home/>}></Route>
+          <Route path='/movie/:id' element={<h2>Movie detaile Element</h2>}></Route>
+          <Route path='/movie/:type' element={<h2>Movie List Pages</h2>}></Route>
+          <Route path='/*' element={<h2>Error page</h2>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
